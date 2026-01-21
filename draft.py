9dc7561aed3ef8cae94f9ac4edfaf1ef91c9a39e
Luiz Eduardo Araújo Brayner <Brayner.luizeduardo@Gmail.com>
2026-01-21 12:45:07 +0000

    gafico com jupiter

--- draft.py
+++ draft.py
@@ -1,3 +1,11 @@
+import plotly.express as px
 from download import download_data
 
-download_data('TSLA')
+df = download_data('AAPL')
+
+px.line(
+    df,
+    x = 'Date',
+    y = 'Close',
+    title='AAPL Stock Price'
+    )
